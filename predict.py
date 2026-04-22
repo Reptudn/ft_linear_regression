5ca40fe244e8bbc0a4c273b78fbf408d745cb2cf
Jonas Kauker <Jonas.Kauker@digits.schwarz>
2026-04-22 17:51:22 +0200

    added types and explicit except values

--- predict.py
+++ predict.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 import shared
 
 try:
     with open("theta.txt", "r") as f:
         theta0 = f.readline().strip()
         theta1 = f.readline().strip()
-except:
+except FileNotFoundError:
     print("Could not read theta.txt!\nUsing default values.")
     theta0 = 0
     theta1 = 0
@@ -14,26 +15,26 @@ except:
 try:
     theta0 = float(theta0)
     theta1 = float(theta1)
-except:
-    print(f"theta0 and theta1 both have to be valid float values!")
+except ValueError:
+    print("theta0 and theta1 both have to be valid float values!")
     sys.exit(1)
 
-print('ft_linear_progression (by jkauker)\n')
+print("ft_linear_progression (by jkauker)\n")
 print(f"Theta0: {theta0}")
 print(f"Theta1: {theta1}")
 print()
 try:
     while True:
-        mileage = input('Enter the car milege: ')
+        mileage = input("Enter the car milege: ")
         try:
             mileage = float(mileage)
-        except:
+        except ValueError:
             print(f"Failed to convert mileage to a useable float value: {mileage}")
             print("Try again...\n")
             continue
 
-        print(f"The estimated price is: {shared.estimatePrice(mileage, theta0, theta1):.2f}eur\n")
-except:
+        print(
+            f"The estimated price is: {shared.estimatePrice(mileage, theta0, theta1):.2f}eur\n"
+        )
+except KeyboardInterrupt:
     print("\n\nThanks for using the price estimator.")
-
-
--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
-import pandas # just for loading the csv effecively
-import matplotlib.pyplot # to show the data (probably not useable in the project submission due to subject restrictions)
+
+import matplotlib.pyplot  # to show the data (probably not useable in the project submission due to subject restrictions)
+import pandas  # just for loading the csv effecively
+
 import shared
 
-theta0 = 0
-theta1 = 0
+theta0: float = 0
+theta1: float = 0
 
 argv = sys.argv
 
@@ -17,22 +19,26 @@ try:
     DATA_CSV = pandas.read_csv(argv[1])
     EPOCHS = int(argv[2])
     L = float(argv[3])
-except:
-    print(f"ERROR: The CSV file need to be accessable from the given path and the epochs(int) and learning(float) var needs to be their reaspective type")
+except ValueError:
+    print(
+        "ERROR: The CSV file need to be accessable from the given path and the epochs(int) and learning(float) var needs to be their reaspective type"
+    )
     sys.exit(1)
 
 # normalizing to prevent overflow
-km_mean = DATA_CSV['km'].mean()
-km_std = DATA_CSV['km'].std()
-price_mean = DATA_CSV['price'].mean()
-price_std = DATA_CSV['price'].std()
+km_mean = DATA_CSV["km"].mean()
+km_std = DATA_CSV["km"].std()
+price_mean = DATA_CSV["price"].mean()
+price_std = DATA_CSV["price"].std()
 
-DATA_CSV['km'] = (DATA_CSV['km'] - km_mean) / km_std
-DATA_CSV['price'] = (DATA_CSV['price'] - price_mean) / price_std
+DATA_CSV["km"] = (DATA_CSV["km"] - km_mean) / km_std
+DATA_CSV["price"] = (DATA_CSV["price"] - price_mean) / price_std
 
-def linear_regression(theta0_curr, theta1_curr, points):
-    theta0_tmp = 0
-    theta1_tmp = 0
+
+# returns the updated theta0 and theta1 values after one epoch
+def linear_regression(theta0_curr, theta1_curr, points) -> tuple[float, float]:
+    theta0_tmp: float = 0.0
+    theta1_tmp: float = 0.0
 
     m = len(points)
     for i in range(m):
@@ -49,15 +55,16 @@ def linear_regression(theta0_curr, theta1_curr, points):
 
     return theta0_tmp, theta1_tmp
 
+
 try:
     for i in range(EPOCHS):
-        if (i % 50 == 0):
+        if i % 50 == 0:
             print(f"{i} - Theta0 is now: {theta0}")
             print(f"{i} - Theta1 is now: {theta1}")
         theta0_tmp, theta1_tmp = linear_regression(theta0, theta1, DATA_CSV)
         theta0 -= theta0_tmp
         theta1 -= theta1_tmp
-except:
+except KeyboardInterrupt:
     print("\n\nTraining interrupted.")
     print("Using the last known values for theta0 and theta1\n\n")
 
@@ -69,13 +76,13 @@ theta0_orig = theta0 * price_std - theta1_orig * km_mean + price_mean
 print(f"Theta0 (original scale): {theta0_orig}")
 print(f"Theta1 (original scale): {theta1_orig}")
 print()
-print(f"Predict the price with: python3 predict.py")
+print("Predict the price with: python3 predict.py")
 
 try:
     with open("theta.txt", "w") as f:
         f.write(f"{theta0_orig}\n")
         f.write(f"{theta1_orig}\n")
-except:
+except FileNotFoundError:
     print("Could not write to theta.txt")
 
 matplotlib.pyplot.scatter(DATA_CSV.km, DATA_CSV.price, color="black")
@@ -83,5 +90,5 @@ matplotlib.pyplot.plot(DATA_CSV.km, theta0 + theta1 * DATA_CSV.km)
 matplotlib.pyplot.title("Linear Regression")
 try:
     matplotlib.pyplot.show()
-except:
+except KeyboardInterrupt:
     print("Bye Bye")
